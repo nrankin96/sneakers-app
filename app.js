cf893f894fb8a04b41185da776965c183fa50e2c
nrankin96 <nykiarankin96@gmail.com>
2022-09-28 23:03:09 -0400

    adding api images

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const ejs = require('ejs');
 const SneaksAPI = require('sneaks-api');
 const sneaks = new SneaksAPI();
 
+let newShoes = document.querySelector('.newShoes');
+
+
 // Setting up express app and body-parser configurations
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -20,6 +23,26 @@ app.get('/', (req, res) => {
     
 });
 
+
+  app.get('/newreleases', (req, res) => {
+    const options = {
+        method: 'GET',
+        url: 'https://v1-sneakers.p.rapidapi.com/v1/sneakers',
+        qs: {limit: '10', releaseYear: '2022'},
+        headers: {
+          'X-RapidAPI-Key': process.env.API_KEY,
+          'X-RapidAPI-Host': 'v1-sneakers.p.rapidapi.com',
+          useQueryString: true
+        }
+      };
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+    
+        console.log(body);
+    });
+  });
+  
+ 
 
 
 app.listen(3000, (req, res) => {
